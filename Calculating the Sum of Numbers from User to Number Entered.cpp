2c9703f847d8c808dcea5b9405782bc960100066
beykiin <132366582+beykiin@users.noreply.github.com>
2024-02-18 14:04:26 +0300

    Add files via upload

---
+++ Calculating the Sum of Numbers from User to Number Entered.cpp
@@ -0,0 +1,33 @@
+﻿// Calculating the Sum of Numbers from User to Number Entered.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num, total = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+    for (size_t i = 1; i <= num; i++) {
+
+        total += i;
+
+    }
+
+    cout << "Some of numbers from " << "1 " << "to " << num << ":"  << total << endl;
+    return 0;
+
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
